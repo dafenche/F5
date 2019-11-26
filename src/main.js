e3936053f476bff86610c45946541ccf56b9e2b7
jojo1117 <xiaojingzhang1117@gmail.com>
2019-11-26 09:42:59 +0800

    'frist'

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'lib-flexible/flexible'
 import router from './router'
-import { Icon } from "vant";
+import { Icon } from "vant"
+import BScroll from 'better-scroll'
+import Swiper from "swiper" 
+import 'swiper/css/swiper.min.css' 
+
+
 
 Vue.use(Icon)
 
